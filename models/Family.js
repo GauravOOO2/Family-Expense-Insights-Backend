ebe1bed52f53b2e50084f0d15d86c817796dab96
Gaurav0002 <gauravking918@gmail.com>
2024-11-26 14:03:17 +0530

    Update

--- models/Family.js
+++ models/Family.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
-const familySchema = new mongoose.Schema({
-  familyId: { type: String, required: true, unique: true },
+const FamilySchema = new mongoose.Schema({
+  familyId: { type: String, required: true },
   income: { type: Number, required: true },
   savings: { type: Number, required: true },
   monthlyExpenses: { type: Number, required: true },
   loanPayments: { type: Number, required: true },
   creditCardSpending: { type: Number, required: true },
   dependents: { type: Number, required: true },
-  financialGoalsMet: { type: Number, required: true }, // Percentage
+  financialGoalsMet: { type: Number, required: true },
 });
 
-module.exports = mongoose.model('Family', familySchema);
+module.exports = mongoose.model('Family', FamilySchema);
--- models/Transaction.js
+++ models/Transaction.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const transactionSchema = new mongoose.Schema({
+const TransactionSchema = new mongoose.Schema({
   familyId: { type: String, required: true },
   memberId: { type: String, required: true },
   transactionDate: { type: Date, required: true },
@@ -8,4 +8,4 @@ const transactionSchema = new mongoose.Schema({
   amount: { type: Number, required: true },
 });
 
-module.exports = mongoose.model('Transaction', transactionSchema);
+module.exports = mongoose.model('Transaction', TransactionSchema);
